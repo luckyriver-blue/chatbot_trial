01f0c8f5eb6d022254322a970345a027f5f16f26
Yoshikawa Midori <yoshikawamidori@Yoshikawas-MacBook-Air.local>
2025-09-15 18:32:06 +0900

    st.secretからos.environに変更しました。

--- config/set_firebase.py
+++ config/set_firebase.py
@@ -1,15 +1,15 @@
-import streamlit as st
+import os
 
 firebase_project_settings = {
   "type": "service_account",
   "project_id": "chatbot-test-42baf",
-  "private_key_id": st.secrets["private_key_id"],
-  "private_key": st.secrets["private_key"],
-  "client_email": st.secrets["client_email"],
-  "client_id": st.secrets["client_id"],
+  "private_key_id": os.environ["private_key_id"],
+  "private_key": os.environ["private_key"],
+  "client_email": os.environ["client_email"],
+  "client_id": os.environ["client_id"],
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+  "client_x509_cert_url": os.environ["client_x509_cert_url"],
   "universe_domain": "googleapis.com"
 }
